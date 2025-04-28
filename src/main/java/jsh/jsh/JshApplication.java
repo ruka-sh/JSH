664a58bc4d0ce6c4e02cd142a057d9a3cf0a36a0
ruka-sh <ori8591y@gmail.com>
2025-04-28 14:21:18 +0900

    init

---
+++ src/main/java/jsh/jsh/JshApplication.java
@@ -0,0 +1,13 @@
+package jsh.jsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/jsh/JshApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.jsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
